3010dc7198aa7554ea5624151dbd89daee0291c3
Danny Feng <dannyfeng1@aol.com>
2021-09-03 10:17:21 -0700

    creating elements

---
+++ src/index.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded", console.log("Webpack is running"))
+
+let charForm = document.getElementById("#character-form")
+
+const createCharacter = function() {
+  
+}
+charForm.addEventListener("submit", createCharacter)
